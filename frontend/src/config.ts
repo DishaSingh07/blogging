8175ae6450aef97306b146f503012499f5541a5a
DishaSingh07 <dishumau@gmail.com>
2024-10-26 02:57:27 +0530

    Your commit message

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,5 +1,5 @@
-export const BACKEND_URL = "http://127.0.0.1:8787"
-// export const BACKEND_URL = "https://backend.dishumau.workers.dev"
+// export const BACKEND_URL = "http://127.0.0.1:8787"
+export const BACKEND_URL = "https://backend.dishumau.workers.dev"
 
 
 //{
